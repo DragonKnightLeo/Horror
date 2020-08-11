99ef6c3515cc7ea6b4b0ede3bab572ce42e22c80
DragonKnightLeo <apcarn0808@gmail.com>
2020-08-11 16:17:38 -0400

    Andrew Third Week Commit

---
+++ Assets/Script/VideoNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class VideoNextScene : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        
+        yield return new WaitForSeconds(10);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
